825d2ea538a30b0d66da30cb31b118b5a34720c7
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-28 23:38:39 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category." }
+  validate :clickbait
+
+  def clickbait
+    if title.present?
+      if !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+        errors.add(:clickbait, "not clickbaity enough.")
+      end
+    end
+  end
+
 end
