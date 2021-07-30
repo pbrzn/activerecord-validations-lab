c4cfde74b670eec4a86c6b95a14e442df486bafe
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-29 22:40:26 -0400

    commit tree bd8e82d613ae8c64e4eb01e15b0b1a85a9aacaf4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,17 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category." }
-  validate :clickbait
-
-  def clickbait
-    if title.present? && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
-      errors.add(:clickbait, "not clickbaity enough.")
-    end
-  end
+  validates_with ClickbaitValidator
+  # validate :clickbait
+  #
+  # def clickbait
+  #   if title.present? && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+  #     errors.add(:clickbait, "not clickbaity enough.")
+  #   end
+  # end
 
 end
